d675dfd737ad6584788fc1a2917be623a295d385
Andree Dantzler <andree.balayssac@icloud.com>
2021-01-12 23:05:11 -0500

    pushing all the files

--- Develop/app.js
+++ Develop/app.js
@@ -11,25 +11,173 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//creating variables per type of employees, empty array for output as well
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const employees = [];
+let outputHtml;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const managerInquiries = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your office number?",
+    name: "officeNumber",
+  },
+];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+const employeeInquiries = [
+  {
+    type: "list",
+    message: "What is role of employee you would like to add?",
+    name: "employeeRole",
+    choices: ["Engineer", "Intern"],
+  },
+];
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const engineerInquiries = [
+  {
+    type: "input",
+    message: "What is the name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the GitHub username?",
+    name: "github",
+  },
+  {
+    type: "list",
+    message: "Would you like to add an employee?",
+    name: "addingEmployee",
+    choices: ["Yes", "No"],
+  },
+];
+
+const internInquiries = [
+  {
+    type: "input",
+    message: "What is the name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the school name?",
+    name: "school",
+  },
+  {
+    type: "list",
+    message: "Would you like to add an employee?",
+    name: "addingEmployee",
+    choices: ["Yes", "No"],
+  },
+];
+
+//separating functions per type to make it clearer
+
+function init() {
+  managerQuestions();
+}
+
+function managerQuestions() {
+  inquirer.prompt(managerInquiries).then(function (answers) {
+    //pushing answers using class
+    employees.push(
+      new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    );
+    console.log(employees);
+    employeeQuestions();
+  });
+}
+
+function employeeQuestions() {
+  inquirer.prompt(employeeInquiries).then(function (answers) {
+    if (answers.employeeRole === "Engineer") {
+      engineerQuestions();
+    } else {
+      internQuestions();
+    }
+  });
+}
+
+function engineerQuestions() {
+  inquirer.prompt(engineerInquiries).then(function (answers) {
+    employees.push(
+      new Engineer(answers.name, answers.id, answers.email, answers.github)
+    );
+    console.log(employees);
+    if (answers.addingEmployee === "Yes") {
+      employeeQuestions();
+    } else {
+      //if no more employees, we can render the page
+      outputHtml = render(employees);
+      writeHtml(outputHtml);
+    }
+  });
+}
+
+function internQuestions() {
+  inquirer.prompt(internInquiries).then(function (answers) {
+    employees.push(
+      new Intern(answers.name, answers.id, answers.email, answers.school)
+    );
+    console.log(employees);
+    if (answers.addingEmployee === "Yes") {
+      employeeQuestions();
+    } else {
+      //generate page
+      outputHtml = render(employees);
+      writeHtml(outputHtml);
+    }
+  });
+}
+
+function writeHtml(data) {
+  fs.mkdir(OUTPUT_DIR, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.error("New directory successfully created.");
+    }
+  });
+  fs.writeFile(outputPath, data, function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
+
+init();
+
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Employee-Summary-Engine
+
+## Description
+  Welcome to the Employee Summary Engine
+
+  I was tasked with creating a software engineering team generator command line application. The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. This assignment must also pass all unit tests. When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user. 
+  ```
+ Below are the aceptance criteria.
+
+## Acceptance Criteria
+
+ As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
+```
+
+## Video
+
+[Watch the video](https://drive.google.com/file/d/1yQ2DKfYcoMtGxJILhHc0yO1MFnT0AgEt/view?usp=sharing)
+
+## Repo URL
+
+  [Repo URL](https://github.com/AndreeDantzler/GoodREADMeGenerator)
+
+## Licenses: 
+
+  MIT
+
+## Tools used:
+  - VS Code
+  - Google
+  - NPM
+  - Node
+
+  ```
+
